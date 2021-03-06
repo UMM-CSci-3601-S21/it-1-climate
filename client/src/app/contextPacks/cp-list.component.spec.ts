9f475d1f7756544273d6d5b8c94db24c4ad217db
Joshua Eklund <eklun124@umn.edu>
2021-03-06 11:05:20 -0600

    Create cp-list component
    This commit creates the cp-list component that is used to display context packs. As of right, it does not actually display context packs, this is just the first step to making that happen. Epic #33
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>, Richard Lussier <lussi036@morris.umn.edu>, Jadyn Sondrol <sondr037@morris.umn.edu>, and Kyle Day <day00090@morris.umn.edu>

---
+++ client/src/app/contextPacks/cp-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CpListComponent } from './cp-list.component';
+
+describe('CpListComponent', () => {
+  let component: CpListComponent;
+  let fixture: ComponentFixture<CpListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CpListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CpListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
