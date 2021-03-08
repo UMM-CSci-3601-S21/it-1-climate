b76b868eb8ef508113b1f4c6dff8deb96eb28a51
Richard Lussier <lussi036@morris.umn.edu>
2021-03-08 16:45:22 -0600

    Add test for schema link
    Added a test to ensure that the $schema variable is the correct link once a ContextPack object is created.

--- server/src/test/java/umm3601/wordRiver/WordRiverControllerSpec.java
+++ server/src/test/java/umm3601/wordRiver/WordRiverControllerSpec.java
@@ -1,5 +1,16 @@
 package umm3601.wordRiver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class WordRiverControllerSpec {
 
+  @Test
+  public void secureSchema() {
+
+    ContextPack schema = new ContextPack();
+    assertEquals("https://raw.githubusercontent.com/kidstech/story-builder/master/Assets/packs/schema/pack.schema.json",
+    schema.$schema);
+  }
 }
