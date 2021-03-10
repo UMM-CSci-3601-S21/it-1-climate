3abcd03eb0630abe775501545b2fc7bd981ea1ca
Joshua Eklund <eklun124@umn.edu>
2021-03-09 18:20:50 -0600

    Implement cp-list.component.ts
    This commit adds the code required to implement cp-list.component.ts. Epic #24 Epic #18
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by: Jadyn Sondrol <sondr037@morris.umn.edu>
    Co-authored-by: Kyle Day <day00090@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>

--- client/src/app/wordRiver/contextPacks/cp-list.component.ts
+++ client/src/app/wordRiver/contextPacks/cp-list.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ContextPack } from '../context-pack';
+import { WordList } from '../word-list';
+import { Word } from '../word';
+import { ContextPackService } from '../context-pack.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cp-list',
   templateUrl: './cp-list.component.html',
   styleUrls: ['./cp-list.component.scss']
 })
-export class CpListComponent implements OnInit {
+export class CpListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public contextPacks: ContextPack[];
 
-  ngOnInit(): void {
+  public name: string;
+  public icon: string;
+  public enabled: boolean;
+  public wordlist: Array<WordList>;
+  getPackSub: Subscription;
+
+  constructor(private packService: ContextPackService) { }
+
+  getPacksFromServer(): void {
+    this.unsub();
+    this.getPackSub = this.packService.getPacks().subscribe(
+      returnedPacks => {
+        this.contextPacks = returnedPacks;
+    }, err => {
+      console.log(err);
+    });
   }
 
+  ngOnInit(): void {
+    this.getPacksFromServer();
+  }
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+
+  unsub(): void {
+    if(this.getPackSub) {
+      this.getPackSub.unsubscribe();
+    }
+  }
 }
