9ddd4a976a51e646bcc764a49acbfda5bf67713b
Joshua Eklund <eklun124@umn.edu>
2021-03-10 14:45:24 -0600

    Test and implement add-cp component
    This commit adds the tests and code for forms for creating an empty context pack. Issue #39 Issue #14 Epic #18
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by: Jadyn Sondrol <sondr037@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { AddUserComponent } from './users/add-user.component';
 import { CpListComponent } from './wordRiver/contextPacks/cp-list.component';
 import { ContextPackService } from './wordRiver/context-pack.service';
 import { CpCardComponent } from './wordRiver/contextPacks/cp-card.component';
+import { AddCpComponent } from './wordRiver/contextPacks/add-cp.component';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -65,6 +66,7 @@ const MATERIAL_MODULES: any[] = [
     AddUserComponent,
     CpListComponent,
     CpCardComponent,
+    AddCpComponent,
   ],
   imports: [
     BrowserModule,
