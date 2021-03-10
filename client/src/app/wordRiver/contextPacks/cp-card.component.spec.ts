99f17c2cb24026c656c909b918e402e4b0dab326
Joshua Eklund <eklun124@umn.edu>
2021-03-09 18:19:31 -0600

    Test cp list component and create/test cp card component
    This commit tests the cp list component, which involved creating and testing the cp card component.  Epic #18 Epic #24
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by: Jadyn Sondrol <sondr037@morris.umn.edu>
    Co-authored-by: Kyle Day <day00090@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>

---
+++ client/src/app/wordRiver/contextPacks/cp-card.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CpCardComponent } from './cp-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { WordList } from '../word-list';
+
+describe('CpCardComponent', () => {
+  let cpCard: CpCardComponent;
+  let fixture: ComponentFixture<CpCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        MatCardModule
+      ],
+      declarations: [ CpCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    const testList: Array<WordList> = [];
+    fixture = TestBed.createComponent(CpCardComponent);
+    cpCard = fixture.componentInstance;
+    cpCard.contextPack = {
+      _id: 'woof',
+      schema: 'https://raw.githubusercontent.com/kidstech/story-builder/master/Assets/packs/schema/pack.schema.json',
+      name: 'canines',
+      icon: '',
+      enabled: true,
+      wordlist: testList
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(cpCard).toBeTruthy();
+  });
+});
--- client/src/app/wordRiver/contextPacks/cp-list.component.spec.ts
+++ client/src/app/wordRiver/contextPacks/cp-list.component.spec.ts
@@ -1,25 +1,72 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
 
 import { CpListComponent } from './cp-list.component';
+import { CpCardComponent } from './cp-card.component';
+import { MockCPService } from 'src/testing/context-pack.service.mock';
+import { ContextPackService } from '../context-pack.service';
+import { ContextPack } from '../context-pack';
+import { WordList } from '../word-list';
+import { Word } from '../word';
+
+const COMMON_IMPORTS: any[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatIconModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
 
 describe('CpListComponent', () => {
-  let component: CpListComponent;
+  let cpList: CpListComponent;
   let fixture: ComponentFixture<CpListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CpListComponent ]
-    })
-    .compileComponents();
+  beforeEach( () => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [ CpListComponent, CpCardComponent ],
+      providers: [{ provide: ContextPackService, useValue: new MockCPService() }]
+    });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CpListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(waitForAsync(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(CpListComponent);
+      cpList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('contains all the packs', () => {
+    expect(cpList.contextPacks.length).toBe(3);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(cpList).toBeTruthy();
   });
 });
