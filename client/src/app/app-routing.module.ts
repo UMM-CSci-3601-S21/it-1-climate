4b88fdf8a17c94204a8e9249f7e9e49a18f5fff3
Joshua Eklund <eklun124@umn.edu>
2021-03-10 15:59:49 -0600

    Fix types in .ts files
    This commit changes arrays into array lists in various fields in context-pack.ts, word-list.ts, word.ts to match the database structure in the server. 
    Co-authored-by: Richard Lussier <lussi036@morris.umn.edu>
    Co-authored-by: Jadyn Sondrol <sondr037@morris.umn.edu>
    Co-authored-by:  Joshua Eklund <eklun124@umn.edu>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { AddUserComponent } from './users/add-user.component';
 import { CpListComponent } from './wordRiver/contextPacks/cp-list.component';
+import { AddCpComponent } from './wordRiver/contextPacks/add-cp.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UserListComponent},
   {path: 'packs', component: CpListComponent},
+  {path: 'packs/new', component: AddCpComponent},
   {path: 'packs/:id', component: CpListComponent},
   {path: 'users/new', component: AddUserComponent},
   {path: 'users/:id', component: UserProfileComponent}
